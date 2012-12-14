9734bb0d0c570c837107327b330876c5484b04a9
maxmilianwoltersdorf <m.woltersdorf@hotmail.de>
2012-12-14 16:30:47 +0100

    smtp gmail

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,11 +1,11 @@
-ActionMailer::Base.smtp_settings = {  
-  :address              => "smtp.gmail.com",  
-  :port                 => 587,  
-  :domain               => "heroku.com",  
-  :user_name            => "maximilian.woltersdorf",  
-  :password             => "sccscc33",  
-  :authentication       => "plain",  
-  :enable_starttls_auto => true  
-} 
+#ActionMailer::Base.smtp_settings = {  
+#  :address              => "smtp.gmail.com",  
+#  :port                 => 587,  
+#  :domain               => "heroku.com",  
+#  :user_name            => "maximilian.woltersdorf",  
+#  :password             => "sccscc33",  
+#  :authentication       => "plain",  
+#  :enable_starttls_auto => true  
+#} 
 
-ActionMailer::Base.delivery_method = :smtp
+#ActionMailer::Base.delivery_method = :smtp
