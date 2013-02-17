08de93d7cb297630d94ea2dc22ba3c60a957a4b5
maxmilianwoltersdorf <m.woltersdorf@hotmail.de>
2013-02-17 15:07:57 +0100

    I18n + language switch angelegt

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :set_locale
 
  
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+ 
   def current_user
-    current_tester || current_customer || current_manager
+    current_consumer || current_customer || current_admin
   end
 
   def after_sign_in_path_for(ressource)
