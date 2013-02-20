9e4794fcdd46ed8d6c821c2788c4643ed4fb550a
maxmilianwoltersdorf <m.woltersdorf@hotmail.de>
2013-02-20 15:54:55 +0100

    newsletter flag

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -2,7 +2,7 @@ class PublicController < ApplicationController
 	layout 'public'
 
 def index
-
+  @request_message = RequestMessage.new unless @request_message
 end
 
 
--- app/controllers/request_messages_controller.rb
+++ app/controllers/request_messages_controller.rb
@@ -1,10 +1,18 @@
 class RequestMessagesController < ApplicationController
 
 	def create
-		@request_message = RequestMessage.create(params[:request_message])
-		StaffMailer.request_message(@request_message).deliver
-		flash[:success] = "Thank you. We have succesfully received your message."
-		redirect_to root_path
+	  @request_message = RequestMessage.create(params[:request_message])
+	  respond_to do |wants|
+	    wants.js
+	  end
+	  
+#		if @request_message.id.present?
+#		  StaffMailer.request_message(@request_message).deliver
+#		  flash[:success] = "Thank you. We have succesfully received your message."
+#		  redirect_to root_path
+#		else 
+#		  render "public/index", :layout => "public"
+#		end
 	end
 
 end
