47417750d97d6e4036a5bf26e4d63617256e6314
maxmilianwoltersdorf <m.woltersdorf@hotmail.de>
2013-02-22 15:21:58 +0100

    pitch & contactform

--- app/controllers/request_messages_controller.rb
+++ app/controllers/request_messages_controller.rb
@@ -2,17 +2,10 @@ class RequestMessagesController < ApplicationController
 
 	def create
 	  @request_message = RequestMessage.create(params[:request_message])
+	  @request_message = RequestMessage.new if @request_message.id?
 	  respond_to do |wants|
 	    wants.js
 	  end
-	  
-#		if @request_message.id.present?
-#		  StaffMailer.request_message(@request_message).deliver
-#		  flash[:success] = "Thank you. We have succesfully received your message."
-#		  redirect_to root_path
-#		else 
-#		  render "public/index", :layout => "public"
-#		end
 	end
 
 end
