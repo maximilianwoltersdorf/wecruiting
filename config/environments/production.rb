44c6fbaaef3e27ee47048f0d1e19ad98a1ef4a0a
maxmilianwoltersdorf <m.woltersdorf@hotmail.de>
2013-03-15 11:59:31 +0100

    smtp settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,7 +50,9 @@ Wecruiting::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
-
+    config.action_mailer.perform_deliveries = true
+    config.action_mailer.delivery_method = :smtp
+    
   # Enable threaded mode
   # config.threadsafe!
 
