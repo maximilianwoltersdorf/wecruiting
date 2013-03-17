11370862e653b2aae0cede67585de7c63162ceb7
SCHRoman <SCHRoman@gmx.de>
2013-03-17 14:22:52 +0100

    WIP - Login-Customization

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,9 @@ module Wecruiting
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.to_prepare do
+      Devise::SessionsController.layout "login"
+    end
   end
 end
